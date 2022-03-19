df313f6b9186c1420a5964ad8a2b8762afc061c6
redark6 <redark66@gmail.com>
2022-03-19 16:02:09 +0100

    page login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from './web-components/home-page/home-page.component';
+import {LoginPageComponent} from './web-components/login-page/login-page.component';
+import {NotFoundPageComponent} from './web-components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: '', component: HomePageComponent },
+  { path: '**', component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './web-components/home-page/home-page.component';
+import { LoginPageComponent } from './web-components/login-page/login-page.component';
+import { NotFoundPageComponent } from './web-components/not-found-page/not-found-page.component';
+import { FooterComponent } from './web-components/global-web-components/footer/footer.component';
+import { NavBarComponent } from './web-components/global-web-components/nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    NotFoundPageComponent,
+    FooterComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatSliderModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
